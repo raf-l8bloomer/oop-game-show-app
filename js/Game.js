aaf7ced4f63d40295fdd4e7ad66fa8d15ec57be8
rafael.lumang <lumang.rafael@gmail.com>
2022-08-06 09:46:41 -0700

    Going back to practice on four-in-a-row

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,31 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 0;
+        this.phrases = [
+            {phrase: 'Alien Superstar'},
+            {phrase: 'America Has A Problem'},
+            {phrase: 'Church Girl'},
+            {phrase: 'Cuff It'},
+            {phrase: 'Plastic Off The Sofa'}
+        ];
+        this.activePhrase = null;
+    }
+
+    startGame(){
+
+    }
+
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+
+    getRandomPhrase () {
+       const randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+       return randomPhrase;    
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+// const phrase = new Phrase();
+// const game = new Game();
+
+// const phrase = new Phrase('Life is like a box of chocolates');
+// console.log(`Phrase - phrase: ${phrase.phrase}`);
+
+const logPhrase = (phrase) => {
+    console.log( phrase.phrase);
+    };
+    const game = new Game();
+    logPhrase(game.getRandomPhrase());
+
+game.getRandomPhrase().addPhraseToDisplay();
